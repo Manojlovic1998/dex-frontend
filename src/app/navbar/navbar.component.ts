627c78fc298d9cadb2f9c3f47a89d198b4a3afeb
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-06-01 15:04:22 +0200

    Add base config using tailwind and daisyUI

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {}
